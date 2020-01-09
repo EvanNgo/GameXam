e830563a46227b72b802b33807cdd9548ed90bd8
EvanNgo <evanngo93@gmail.com>
2020-01-09 19:08:46 +0900

    Create bullet.js

---
+++ tower/bullet.js
@@ -0,0 +1,60 @@
+import * as Const from "../../const.js";
+
+export default class Bullet {
+  constructor(tower) {
+    this.tower = tower;
+    this.target = this.tower.target;
+    this.position = {
+      x: this.tower.position.x,
+      y: this.tower.position.y
+    };
+    this.speed = Const.GAME_FPS/3;
+    this.isDraw = true;
+  }
+  hitEnemy() {
+    this.target.hitByBullet(this.tower.dame);
+  }
+  update() {
+    if (this.target != null) {
+      if (this.getDistance(this.position, this.target.position) > Const.GOUND_SIZE/2.5) {
+        if (Math.abs(this.position.x - this.target.position.x) < this.speed) {
+          this.position.x = this.target.position.x;
+        } else {
+          if (this.position.x > this.target.position.x) {
+            this.position.x -= this.speed;
+          } else {
+            this.position.x += this.speed;
+          }
+        }
+
+        if (Math.abs(this.position.y - this.target.position.y) < this.speed) {
+          this.position.y = this.target.position.y;
+        } else {
+          if (this.position.y > this.target.position.y) {
+            this.position.y -= this.speed;
+          } else {
+            this.position.y += this.speed;
+          }
+        }
+      } else {
+        this.hitEnemy();
+        this.destroy();
+      }
+    } else {
+      this.destroy();
+    }
+  }
+  destroy() {
+    this.isDraw = false;
+  }
+  getDistance(start, end) {
+    let distance = Math.sqrt(Math.pow(start.x - end.x, 2) + Math.pow(start.y - end.y, 2));
+    return distance;
+  }
+  draw() {
+    this.tower.board.game.ctx.fillStyle = "rgb(0, 0, 0)";
+    this.tower.board.game.ctx.beginPath();
+    this.tower.board.game.ctx.arc(this.position.x, this.position.y, Const.GOUND_SIZE/6, 0, 2 * Math.PI);
+    this.tower.board.game.ctx.fill();
+  }
+}
