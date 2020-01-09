e9d363d9eb8ec1faf996f9887cc1eb50f0add965
EvanNgo <evanngo93@gmail.com>
2020-01-09 19:06:40 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,35 +1,63 @@
-import Game from "./game.js";
-document.addEventListener("DOMContentLoaded", function() {
-  var PIXEL_RATIO = (function () {
-    var ctx = document.createElement("canvas").getContext("2d"),
-        dpr = window.devicePixelRatio || 1,
-        bsr = ctx.webkitBackingStorePixelRatio ||
-              ctx.mozBackingStorePixelRatio ||
-              ctx.msBackingStorePixelRatio ||
-              ctx.oBackingStorePixelRatio ||
-              ctx.backingStorePixelRatio || 1;
-    return dpr / bsr;
-  })();
+import * as Const from "./const.js";
+import Board from "./board.js";
 
-  var root = document.getElementById('root');
-  var game = new Game(root, PIXEL_RATIO);
+export default class Game {
+  constructor(root, ratio) {
+    this.root = root;
+    this.ratio = ratio;
+    this.canvas = null;
+    this.rect = null;
+    this.ctx = null;
+    this.isAlive = null;
+    this.board = null;
 
-  game.canvas.addEventListener("mousemove",onMouseMove,false);
-  function onMouseMove(event) {
-    var mousePosition = {
-      x: event.pageX - game.rect.left,
-      y: event.pageY - game.rect.top
-    }
-    game.onMouseMove(mousePosition);
+    this.init();
+  }
+  init() {
+    this.canvas = document.createElement('canvas');
+    this.ctx = this.canvas.getContext('2d');
+    this.canvas.width = Const.GAME_WIDTH * this.ratio;
+    this.canvas.height = Const.GAME_HEIGHT * this.ratio;
+    this.canvas.style.width = Const.GAME_WIDTH + "px";
+    this.canvas.style.height = Const.GAME_HEIGHT + "px";
+    this.ctx.setTransform(this.ratio, 0, 0, this.ratio, 0, 0);
+    this.root.appendChild(this.canvas);
+    this.isAlive = true;
+    this.rect = this.canvas.getBoundingClientRect();
+
+    //create board
+    this.board = new Board(this);
   }
 
-  game.canvas.addEventListener("mousedown",onMouseClick,false);
-  function onMouseClick(event) {
-    var clickPosition = {
-      x: event.pageX - game.rect.left,
-      y: event.pageY - game.rect.top
-    }
-    game.onMouseClick(clickPosition);
+  onMouseMove(position) {
+    this.board.onMouseMove(position);
   }
-  game.start();
-});
+
+  onMouseClick(position) {
+    this.board.onMouseClick(position);
+  }
+
+  isAlive() {
+    return this.isAlive;
+  }
+  clear() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+  update() {
+    this.board.update();
+  }
+  draw() {
+    this.board.draw();
+  }
+  loop() {
+    if (() => this.isAlive()) {
+      this.clear();
+      this.update();
+      this.draw();
+      setTimeout(() => this.loop(), 1000/Const.GAME_FPS);
+    }
+  }
+  start() {
+    this.loop();
+  }
+}
